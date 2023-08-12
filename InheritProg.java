dada6c0d094940968a1ebb629f52218bd617a8f2
Sarvanan N <nsaravanan1204@gmail.com>
2023-08-12 10:23:41 +0530

    "Inherit Example"

---
+++ InheritProg.java
@@ -0,0 +1,30 @@
+class Calculator{
+
+    public int add(int a,int b){
+        return a+b;
+    }
+
+    public int sub(int a,int b){
+        return a-b;
+    }
+
+
+
+}
+
+class AdvCalculator extends Calculator{
+    public int mul(int a,int b){
+        return a*b;
+    }
+
+    public int div(int a,int b){
+        return a/b;
+    }
+}
+
+public class InheritProg {
+    public static void main(String[] args) {
+        AdvCalculator acl = new AdvCalculator();
+        System.out.println("AdvCalculator Inherit Result :" + acl.add(2,3));
+    }
+}
