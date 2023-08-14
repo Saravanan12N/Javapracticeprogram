947b2dbc620457dd97a0c26b1b8ada3838f7c327
Sarvanan N <nsaravanan1204@gmail.com>
2023-08-14 16:16:54 +0530

    Lambda Experisson Example

---
+++ PracProgram/LambdaExp.java
@@ -0,0 +1,18 @@
+package PracProgram;
+
+@FunctionalInterface
+interface Lamda{
+    void show();
+}
+
+
+public class LambdaExp {
+    public static void main(String[] args) {
+        Lamda bb  = () -> System.out.println("in Void show");
+        bb.show();
+    }
+
+}
+/*
+in Void show
+ */
