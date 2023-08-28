d6b88fcb4828f5895488bdd7983335443537ed16
Sarvanan N <nsaravanan1204@gmail.com>
2023-08-28 22:01:37 +0530

    Log Example Program

---
+++ PracProgram/LogDemo.java
@@ -0,0 +1,16 @@
+package PracProgram;
+import java.math.*;
+
+import static java.lang.Integer.parseInt;
+
+public class LogDemo {
+    public static void main(String[] args) {
+        double value = Math.log10(100);
+        System.out.println(value);
+
+    }
+}
+
+/*
+2.0
+ */
